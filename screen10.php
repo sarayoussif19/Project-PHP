f0f0739fd4ac90630147a60b46b2e1d92f54fdf0
sarayoussif19 <sarayoussif666@gmail.com>
2022-07-01 09:56:20 +0200

    sara
    sara added this as files

---
+++ screen10.php
@@ -0,0 +1,12 @@
+<?php include 'navbar.html';?>
+<h3>Orders</h3>
+<table class="table table-border">
+    <tr>
+        <th>order date</th>
+        <th>name</th>
+        <th>room</th>
+        <th>ext</th>
+        <th>action</th>
+    </tr>
+    
+</table>
