2a0ed8b106d33dd7d086dead452825fa73bc95c2
sarayoussif19 <sarayoussif666@gmail.com>
2022-07-04 11:13:40 +0200

    sara added these files

--- screen10.php
+++ screen10.php
@@ -1,17 +1,39 @@
- <?php // include 'navbar.html';?> 
+<?php  include 'navbar.html'?> 
 <?php 
+//connection DB
 $db='mysql:host=localhost;dbname=php_project';
 $conn= new PDO($db,'root','');
-var_dump($conn);
-?>
-<!-- <h3>Orders</h3>
-<table class="table table-border">
-    <tr>
+$Dfrom = $_REQUEST['datefrom'];
+$Dto = $_REQUEST['dateto'];
+$id = $_REQUEST['username'];
+ 
+// $sql->fetchAll();
+echo '<h3>orders</h3>';
+$query3="SELECT orders.order_date , user.name , user.room_no , user.ext , orders.status FROM `user` JOIN `orders` ON  user.user_id =$id /* && oreders.date_from=$Dfrom && orders.date_to=$Dto*/ ";
+$sql2=$conn->prepare($query3);
+$sql2->execute();
+while($row = $sql2->fetch(PDO::FETCH_ASSOC)){
+
+echo ' <table class="table shadow table-border">
+<tr>
         <th>order date</th>
         <th>name</th>
         <th>room</th>
         <th>ext</th>
         <th>action</th>
-    </tr>
-    
-</table> -->
+    </tr>';
+
+    echo'<tr>';
+    echo'<td>'.$row['order_date'].'</td>';
+    echo'<td>'.$row['name'].'</td>';
+    echo'<td>'.$row['room_no'].'</td>';
+    echo'<td>'.$row['ext'].'</td>';
+    echo'<td>'.$row['status'].'</td>';
+    echo'</tr>';
+    echo "<tr><img src='./images/Dwarf_mongoose_Korkeasaari_zoo.jpg' width='100px' ></tr>";
+   
+   echo'</table>';
+}
+  // JOIN orders ON product.product_id =orders.product_id WHERE orders.user_id =1 
+?>
+
--- screen9.php
+++ screen9.php
@@ -1,3 +1,73 @@
-<?php include 'navbar.html';?>
+<?php include 'navbar.html';
+$db='mysql:host=localhost;dbname=php_project';
+$conn= new PDO($db,'root','');
+// 
+$query="SELECT * FROM user  ";
+$sql=$conn->prepare($query);
+$sql->execute();
+// $sql->fetchAll();
+echo '<h3>orders</h3>';
+
+ 
+echo '
+<form action="screen10.php" method="post"> 
+    <label for="datefrom">date from</label>
+    <input type="time" name="datefrom" id="datefrom" name="datefrom">
+    <label for="dateto">date from</label>
+    <input type="time" placeholder="date from" id="dateto" name="dateto">
+    <select name="username" id="username">';
+    while($row = $sql->fetch(PDO::FETCH_ASSOC)){
+       echo '<option value='.$row['user_id'].'>'. $row['name'].'</option>' ;
+
+};
+  echo '  </select><br><br>
+  <button type="submit" class="btn btn-success text-center">show details</button>
+</form>';
+echo ' <table class="table table-border shadow container">
+<tr>
+        <th>Name</th>
+        <th>tatal amount</th>
+
+    </tr>
+ 
+   ';
+   $query2="SELECT user.name ,orders.amount FROM `user` JOIN `orders` ON user.user_id = orders.user_id ";
+$sql2=$conn->prepare($query2);
+$sql2->execute();
+while($rows = $sql2->fetch(PDO::FETCH_ASSOC)){
+
+    echo'<tr>';
+    echo'<td>'. $rows['name'].'</td>';
+    echo'<td>'. $rows['amount'].'</td>';
+//    echo'<td>'.$row['user.name'].'</td>';
+    
+    echo'</tr>';
+   }
+   echo'</table><br><br>';
+//////////
+   echo ' <table class="table table-border shadow container">
+<tr>
+        <th>Order date</th>
+        <th> amount</th>
+
+    </tr>
+ 
+   ';
+   $query2="SELECT * FROM `orders`  ";
+$sql2=$conn->prepare($query2);
+$sql2->execute();
+while($rows = $sql2->fetch(PDO::FETCH_ASSOC)){
+
+    echo'<tr>';
+    echo'<td>'. $rows['order_date'].'</td>';
+    echo'<td>'. $rows['amount'].'</td>';
+//    echo'<td>'.$row['user.name'].'</td>';
+    
+    echo'</tr>';
+   }
+   echo'</table>';
+
+   //WHERE orders.user_id =1 
+?>
 
 
